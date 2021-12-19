9a4fde3f7bd589a21cbe7c91e40a0ed7d26b59ea
Pranav <prajpran@gmail.com>
2021-12-19 14:29:48 +0530

    init commit

--- src/interpreter/interpret.cpp
+++ src/interpreter/interpret.cpp
@@ -3,10 +3,7 @@
 
 Interpreter::Interpreter(std::string fileName){
 	this->inFile = std::fstream(fileName);
-	uint8_t temp = 0;
-	for(auto item:this->memoryBuffer){
-		std::cout<<(int)item<<"--";
-	}
+	this->memoryPointer = 0;
 }
 
 
